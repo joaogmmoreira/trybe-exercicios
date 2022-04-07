cba4cefbde5a40c40dedc79a2d4320e9400bb92f
Joao Moreira <jgmoreira1809@gmail.com>
2022-04-07 05:22:26 -0700

    exercício 11 completo

--- bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio-11.js
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio-11.js
@@ -1,52 +1,52 @@
-let salary = 1500;
+let bruteSalary = 15000;
 let INSS;
 let IR;
 
 //INSS
-if(salary >= 5189.82){
+if(bruteSalary >= 5189.82){
     INSS = 570.88;
-    console.log('INSS = ' + INSS);
+    console.log('Valor do INSS = ' + INSS);
 
-}else if(salary >= 2594.93){
-    INSS = (salary * 11) / 100;
-    console.log('INSS = ' + INSS);
+}else if(bruteSalary >= 2594.93){
+    INSS = (bruteSalary * 11) / 100;
+    console.log('Valor do INSS = ' + INSS);
 
-}else if(salary >= 1556.95){
-    INSS = (salary * 9) / 100;
-    console.log('INSS = ' + INSS);
+}else if(bruteSalary >= 1556.95){
+    INSS = (bruteSalary * 9) / 100;
+    console.log('Valor do INSS = ' + INSS);
 
 }else{
-    INSS = (salary * 8) / 100;
-    console.log('INSS = ' + INSS);
+    INSS = (bruteSalary * 8) / 100;
+    console.log('Valor do INSS = ' + INSS);
 }
 
-let brute = salary - INSS;
-console.log('Salário Bruto = ' + brute)
+let salaryMinusInss = bruteSalary - INSS;
+console.log('Salário menos INSS = ' + salaryMinusInss)
 
 
 //IR
-if(brute >= 4664,68){
-    IR = [(brute * 27.5) / 100] - 869,36;
-    console.log('IR = ' + IR);
+if(salaryMinusInss >= 4664.68){
+    IR = [(salaryMinusInss * 27.5) / 100] - 869.36;
+    console.log('Valor do IR = ' + IR);
 
-}else if(brute >= 3751.06){
-    IR = [(brute * 22.5) / 100] - 636.13;
-    console.log('IR = ' + IR);
+}else if(salaryMinusInss >= 3751.06){
+    IR = [(salaryMinusInss * 22.5) / 100] - 636.13;
+    console.log('Valor do IR = ' + IR);
 
-}else if(brute >= 2826.65){
-    IR = [(brute * 15) / 100] - 354.8;
-    console.log('IR = ' + IR);
+}else if(salaryMinusInss >= 2826.65){
+    IR = [(salaryMinusInss * 15) / 100] - 354.8;
+    console.log('valor do IR = ' + IR);
 
-}else if(brute >= 1903.99){
-    IR = [(brute * 7.5) / 100] - 142.8;
-    console.log('IR = ' + IR);
+}else if(salaryMinusInss >= 1903.99){
+    IR = [(salaryMinusInss * 7.5) / 100] - 142.8;
+    console.log('Valor do IR = ' + IR);
 
 }else{
     IR = 0;
-    console.log('IR = ' + IR);
+    console.log('Valor do IR = ' + IR);
 }
 
-let netSalary = salary - INSS - IR;
+let netSalary = bruteSalary - INSS - IR;
 
 
 console.log('Salário líquido = ' + netSalary);
