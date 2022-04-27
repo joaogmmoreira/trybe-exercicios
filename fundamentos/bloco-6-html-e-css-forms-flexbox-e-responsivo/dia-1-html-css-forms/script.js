051d2650a3c02f020ea568b3d327c73cd86737d2
Joao Moreira <jgmoreira1809@gmail.com>
2022-04-27 09:30:38 -0700

    pronto?

---
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -0,0 +1,21 @@
+// SELETORES
+// const INPUT_TEXT = document.querySelector("#input-text");
+// const INPUT_CHECKBOX = document.querySelector("#input-checkbox");
+// const HREF_LINK = document.querySelector("#href");
+
+
+// HREF_LINK.addEventListener('click', oi);
+// INPUT_CHECKBOX.addEventListener('click', oi);
+// INPUT_TEXT.addEventListener('keypress', oi);
+
+// function oi (event) {
+//     event.preventDefault();
+// }
+
+let button = document.querySelector('.buttons');
+
+button.addEventListener('click', lala);
+
+function lala (event) {
+    event.preventDefault();
+}
